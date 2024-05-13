ccf82a11d0fca094020b79a45a065ba99e2304be
LucasNehemia2403 <ruhan2403@gmail.com>
2024-05-13 21:59:19 +0700

    yang udah di update (mungkin...... :) )

---
+++ Konverter Suhu.py
@@ -0,0 +1,30 @@
+celcius = int(input("celcius= "))
+fahrenheit = celcius * 9/5 + 32
+
+
+print(f"suhu {celcius} C sama dengan {fahrenheit} F")
+
+fahrenheit = int(input("fahrenheit= "))
+celcius = (fahrenheit - 32) * 5/9
+
+print(f"suhu {fahrenheit} F sama dengan {celcius} C")
+
+kelvin = int(input("kelvin= "))
+celcius = kelvin - 273.15 
+
+print(f"suhu {kelvin}) K sama dengan {celcius} C")
+
+celcius = int(input("celcius= "))
+kelvin = celcius + 273.15
+
+print(f"suhu {celcius} C sama dengan {kelvin} K")
+
+fahrenheit = int(input("fahrenheit= "))
+kelvin = (fahrenheit - 32) x 5/9 + 273.15
+
+print(f"suhu {fahrenheit} F sama dengan {kelvin} K")
+
+kelvin = int(input("kelvin= ")
+fahrenheit = (kelvin - 273.15) x 9/5 + 32 
+
+print(f"suhu {kelvin} K sama dengan {fahrenheit} F")
